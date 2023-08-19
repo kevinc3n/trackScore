958e8a9a372e991c14ae9574b59bf9114e23e3e8
kevinc3n <92806132+kevinc3n@users.noreply.github.com>
2023-08-18 20:30:54 -0400

    ATTEMPT TO FIX DEPLOYMENT #2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    webpack: (config) => {
+      config.resolve.alias['@'] = __dirname;
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
