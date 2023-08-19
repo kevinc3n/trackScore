d77792c14b38fa9c66101cc9fe7559e43023a7ed
kevinc3n <92806132+kevinc3n@users.noreply.github.com>
2023-08-18 21:09:52 -0400

    ATTEMPT TO FIX DEPLOYMENT #6

--- components/swiperComponent.js
+++ components/swiperComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper.min.css';
+import 'swiper/css';
 import { Righteous } from '@next/font/google';
 import Typography from '@mui/material/Typography';
 
